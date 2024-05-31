000af85ed08ece99402947d9523f3ee4a1827547
Aritra Bera <aritrabera69@gmail.com>
2024-05-31 23:59:10 +0530

    02_variableAndDataTypes.cpp

---
+++ 01_Basic/02_variableAndDataTypes.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a=10;
+    cout<<"The value of a is:- "<<a<<endl;
+
+    char c='A';
+    cout<<"The value of c is :- "<<c<<endl;
+    
+    float f=1.22;
+    cout<<"The value of f is :- "<<f<<endl;
+    
+    double d=1.122222;
+    cout<<"The value of d is :- "<<d<<endl;
+    
+    bool b=true;
+    cout<<"The value of b is :- "<<b<<endl;
+    
+    bool bl=false;
+    cout<<"The value of bl is :- "<<bl<<endl;
+
+    int size=sizeof(d);
+    cout<<"Size of a is "<<sizeof(d);
+    return 0;
+}
