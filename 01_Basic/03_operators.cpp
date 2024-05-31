ee09e3fd4915434d9189f4dd424a558af27919ed
Aritra Bera <aritrabera69@gmail.com>
2024-06-01 00:07:41 +0530

    comparison operators/Relational Operators

--- 01_Basic/03_operators.cpp
+++ 01_Basic/03_operators.cpp
@@ -6,7 +6,7 @@ int main ()
 
     //Arithmetic Operators
     cout<<"Arithmetic Operators"<<endl;
-    
+
     cout<<"The value of a+b is :- "<<a+b<<endl;
     cout<<"The value of a-b is :- "<<a-b<<endl;
     cout<<"The value of a*b is :- "<<a*b<<endl;
@@ -14,6 +14,25 @@ int main ()
     cout<<"The value of a%b is :- "<<a%b<<endl;
     cout<<"The value of ++a is :- "<<++a<<endl;
     cout<<"The value of --a is :- "<<--a<<endl;
+
+    cout<<endl;
+    cout<<endl;
+
+    //comparison operators/Relational Operators
+    cout<<"Comparison Operators/Relational Operators"<<endl;
+    
+    bool first = (a>b);
+    cout<<"The value of first is :- "<<first<<endl;
+    bool second = (a<b);
+    cout<<"The value of second is :- "<<second<<endl;
+    bool third =(a==b);
+    cout<<"The Value of third is "<<third<<endl;
+    bool fourth =(a!=b);
+    cout<<"The Value of fourth is "<<fourth<<endl;
+    bool fifth =(a>=b);
+    cout<<"The Value of fifth is "<<fifth<<endl;
+    bool sixth =(a<=b);
+    cout<<"The Value of sixth is "<<sixth<<endl;
 
     cout<<endl;
     cout<<endl;
