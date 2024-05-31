947d758e8823d577b3d89f9526e16f78a5388029
Aritra Bera <aritrabera69@gmail.com>
2024-06-01 00:04:38 +0530

    Arithmetic Operators

---
+++ 01_Basic/03_operators.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+int main ()
+{
+    int a=30,b=10;
+
+    //Arithmetic Operators
+    cout<<"Arithmetic Operators"<<endl;
+    
+    cout<<"The value of a+b is :- "<<a+b<<endl;
+    cout<<"The value of a-b is :- "<<a-b<<endl;
+    cout<<"The value of a*b is :- "<<a*b<<endl;
+    cout<<"The value of a/b is :- "<<a/b<<endl;
+    cout<<"The value of a%b is :- "<<a%b<<endl;
+    cout<<"The value of ++a is :- "<<++a<<endl;
+    cout<<"The value of --a is :- "<<--a<<endl;
+
+    cout<<endl;
+    cout<<endl;
+}
