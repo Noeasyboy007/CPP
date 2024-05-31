8f01f9b251774d1cc566e6e25e314f86e89a7eae
Aritra Bera <aritrabera69@gmail.com>
2024-06-01 00:44:09 +0530

    10_break.cpp

---
+++ 01_Basic/10_break.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    cout<<"enter a number"<<endl;
+    cin>>n;
+    for (int i = 1; i <= n; i++)
+
+    {
+        cout<<i<<endl;
+
+        if (i==5)
+        {
+            break;
+        }
+        
+    }
+}
